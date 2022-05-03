8d2abd1aeb304b91fb87eaf122d633e2f8d20ecb
alexander-bratt-nti-johanneberg <74712627+alexander-bratt-nti-johanneberg@users.noreply.github.com>
2022-05-03 15:22:17 +0200

    Lite Komplettering

--- app.rb
+++ app.rb
@@ -7,30 +7,30 @@ include Model
 enable :sessions 
 
 
-before ('/titles/:id/rate') do
-  if session[:id] == nil
-    redirect('/error')
-  end
-end
-
-before ('/titles/:id/edit') do
-  if session[:id] == nil || session[:auth] != 2
-    redirect('/error')
-  end
-end
-
-before ('/titles/:id/delete') do
-  if session[:id] == nil || session[:auth] != 2
-    redirect('/error')
-  end
-end
-
-before ('/titles/new') do
-  if session[:id] == nil
-    redirect('/error')
-  end
-end
-
+#before ('/titles/:id/rate') do
+#  if session[:id] == nil
+#    redirect('/error')
+#  end
+#end
+#
+#before ('/titles/:id/edit') do
+#  if session[:id] == nil || session[:auth] != 2
+#    redirect('/error')
+#  end
+#end
+#
+#before ('/titles/:id/delete') do
+#  if session[:id] == nil || session[:auth] != 2
+#    redirect('/error')
+#  end
+#end
+#
+#before ('/titles/new') do
+#  if session[:id] == nil
+#    redirect('/error')
+#  end
+#end
+#
 get('/error') do 
   slim(:error)
 end
@@ -54,9 +54,28 @@ get('/logout') do
 end
 
 post('/login') do
-  username = params[:username]
-  password = params[:password]
-  login_user(username,password)
+  if session[:timeLogged] == nil
+    session[:timeLogged] = 0
+  end
+
+  logTime =  checkTime(session[:timeLogged])
+  session[:timeLogged] = Time.now.to_i
+  
+  if logTime
+
+    username = params[:username]
+    password = params[:password]
+    loginResult = login_user(username,password)
+    if loginResult[:status] == true
+      session[:id] = loginResult[:id]
+      session[:auth] = loginResult[:auth]
+      redirect('/titles')
+    else 
+      "Fel Användarnamn/lösenord"
+    end
+  else
+    redirect('/showlogin')
+  end
 end
 
 get('/titles') do
@@ -69,24 +88,53 @@ get('/titles/new') do
     slim(:"titles/new")
 end
 
-post('/titles/new') do
+post('/titles/') do
   producer_id = params[:producer_id].to_i
   title = params[:title]
   genre_id = params[:genre_id].to_i
   user_id = session[:id]
-  create_new_movie(producer_id, title, genre_id, user_id)
+  create_new_movieResult = create_new_movie(producer_id, title, genre_id, user_id)
+
+  if create_new_movieResult[:status] == true
+    redirect('/titles')
+  else 
+    "FEL"
+  end
 end
 
 post('/titles/:id/delete') do
   id = params[:id].to_i
-  delete_movie(id)
+  owner = check_owner(id)
+  if owner["user_id"] == session[:id]
+    delete_movie(id)
+    redirect('/titles')
+  else
+    "Du äger inte den här filmen!"
+  end
 end
 
-post('/users/new') do
-  username = params[:username]
-  password = params[:password]
-  password_confirm = params[:password_confirm]
-  register_user(username, password, password_confirm)
+post('/users/') do
+  if session[:timeLogged] == nil
+    session[:timeLogged] = 0
+  end
+
+  logTime =  checkTime(session[:timeLogged])
+  session[:timeLogged] = Time.now.to_i
+  
+  if logTime
+
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    registerResult = register_user(username, password, password_confirm)
+    if registerResult[:status] == true
+      redirect('/')
+    else 
+      "Användarnamnet/Lösenord är för långt/Lösenorden matchade inte."
+    end
+  else
+    redirect('/register')
+  end
 end
 
 
@@ -100,7 +148,13 @@ post('/titles/:id/update') do
   id = params[:id].to_i
   title = params[:title]
   producer_id = params[:ProducerId]
-  edit_movie(id, title, producer_id)
+  genre_id = params[:GenreId]
+  edit_movieResult = edit_movie(id, title, producer_id, genre_id)
+  if edit_movieResult[:status] == true
+    redirect('/titles')
+  else
+    "Du har gjort fel"
+  end
 end
 
 get('/titles/:id') do
@@ -122,5 +176,10 @@ post('/titles/:id/rated') do
   title_id = params[:id].to_i
   rating = params[:rating].to_i
   user_id = session[:id]
-  rate_movie(title_id, rating, user_id)
+  rate_movieResult = rate_movie(title_id, rating, user_id)
+  if rate_movieResult[:status] == true
+    redirect('/titles')
+  else
+    "Rating Större än 10/Du har inte använt en siffra"
+  end
 end
--- model.rb
+++ model.rb
@@ -10,58 +10,70 @@ module Model
       db = connect_to_db()
       result = db.execute("SELECT * FROM users WHERE username = ?",username).first
       if result == nil
-        redirect("https://www.youtube.com/watch?v=xvFZjo5PgG0")
+        return {status:false}
       end
       pwdigest = result["pwdigest"]
       id = result["id"]
       auth = result["authorization"]
 
       if BCrypt::Password.new(pwdigest) == password
-        session[:id] = id
-        session[:auth] = auth
-        redirect('/titles')
+        return {id:id, auth:auth, status:true}
       else
-        redirect("https://www.youtube.com/watch?v=xvFZjo5PgG0")
+        return {status:false}
       end
   end
 
   def create_new_movie(producer_id, title, genre_id, user_id)
       db = connect_to_db()
-      db.execute("INSERT INTO titles (name, producer_id, genre_id, user_id) VALUES (?,?,?,?)",title,producer_id,genre_id,user_id)
-      redirect('/titles')
+      if title.length <= 20 && (producer_id <= 2 || producer_id > 0)  && (genre_id <= 3 || genre_id > 0)
+        db.execute("INSERT INTO titles (name, producer_id, genre_id, user_id) VALUES (?,?,?,?)",title,producer_id,genre_id,user_id)
+        return {status:true}
+      else
+        return {status:false}
+      end
+  end
+  def check_owner(id)
+      db = connect_to_db()
+      db.execute("SELECT user_id FROM titles WHERE id = ?", id).first
   end
 
   def delete_movie(id)
       db = connect_to_db()
       db.execute("DELETE FROM titles WHERE id = ?",id)
-      redirect('/titles')
+      db.execute("DELETE FROM users_titles WHERE title_id = ?",id)
   end
 
   def register_user(username, password, password_confirm)
 
-    if password == password_confirm && username.length <= 12
+    if (password == password_confirm && password.length <= 12 && password.length > 0) && (username.length > 0 && username.length <= 12 )
       password_digest = BCrypt::Password.create(password)
       authorization = 1
       db = connect_to_db()
       db.execute("INSERT INTO users (username,pwdigest,authorization) VALUES (?,?,?)",username,password_digest,authorization)
-      redirect('/')
-
+      return {status:true}
     else 
-
-      "Användarnamnet är för långt/Lösenorden matchade inte."
+      return {status:false}
     end
   end
 
-  def edit_movie(id, title, producer_id)
+  def edit_movie(id, title, producer_id, genre_id)
       db = connect_to_db()
-      db.execute("UPDATE titles SET name = ?, producer_id = ? WHERE id = ?", title, id, producer_id)
-      redirect('/titles')
+      if title.length <= 20 && (producer_id <= 2 || producer_id > 0)  && (genre_id <= 3 || genre_id > 0)
+        db.execute("UPDATE titles SET name = ?, producer_id = ?, genre_id = ? WHERE id = ?", title, id, producer_id, genre_id)
+        return {status:true}
+      else
+        return {status:false}
+      end
   end
 
   def rate_movie(title_id, rating, user_id)
       db = connect_to_db()
-      db.execute("INSERT INTO users_titles (user_id,title_id,rating) VALUES (?,?,?)", user_id,title_id,rating).first
-      redirect('/titles')
+      if rating <= 10 && rating.class == Integer
+        db.execute("INSERT INTO users_titles (user_id,title_id,rating) VALUES (?,?,?)", user_id,title_id,rating).first
+        return {status:true}
+      else
+        return {status:false}
+      end
   end
 
   def goto_edit_movie(id)
@@ -94,4 +106,12 @@ module Model
       db.execute("SELECT AVG(rating) FROM users_titles WHERE title_id = ?", id).first
   end
   
+end
+
+def checkTime(latestTime)
+
+  timeDiff = Time.now.to_i - latestTime
+
+  return timeDiff > 1.5
+
 end
